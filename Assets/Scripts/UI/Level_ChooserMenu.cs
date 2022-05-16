e1a9dca2eec9fa6dc80181c1e1f09ec206cfc478
Steven Hall <stevenbryanb.work@gmail.com>
2022-05-16 18:04:05 +0100

    Level Chooser Code Update, Cleanup & Scene management

--- Assets/Scripts/UI/Level_ChooserMenu.cs
+++ Assets/Scripts/UI/Level_ChooserMenu.cs
@@ -5,139 +5,130 @@ public class Level_ChooserMenu : MonoBehaviour
 {
 
     //  Menu Game Object
-    [Header("Menus config")]
+    [Header("Menu")]
     [SerializeField]
     private GameObject MainLevelChooser, Menu1, Menu2, Menu3;
 
-    // new menu voids
 
-    public void Menu_1()
-    {
-        MainLevelChooser.SetActive(false);
-        Menu1.SetActive(true);
+    [Header("Back Buttons")]
+    [SerializeField]
+    private GameObject BackButtonInMENUS,BackToStart;
 
-    }
 
-    public void Menu_2()
-    {
-        MainLevelChooser.SetActive(false);
-        Menu2.SetActive(true);
-    }
-
-    public void Menu_3()
-    {
-        MainLevelChooser.SetActive(false);
-        Menu3.SetActive(true);
-
-    }
-
-    public void BackToMenu()
+    // FAIL SAFE CODE
+    public void Start()
     {
         MainLevelChooser.SetActive(true);
 
         Menu1.SetActive(false);
         Menu2.SetActive(false);
         Menu3.SetActive(false);
+
+        BackButtonInMENUS.SetActive(false);
+
+    }
+
+    // Normal Code
+
+    public void Menu_1() // Level Nature Chooser
+    {
+        MainLevelChooser.SetActive(false);
+        Menu1.SetActive(true);
+
+        BackButtonInMENUS.SetActive(true);
+    }
+
+    public void Menu_2() // Level Desert Chooser
+    {
+        MainLevelChooser.SetActive(false);
+        Menu2.SetActive(true);
+
+        BackButtonInMENUS.SetActive(true);
+    }
+
+    public void Menu_3() // Level Halloween Chooser
+    {
+        MainLevelChooser.SetActive(false);
+        Menu3.SetActive(true);
+
+        BackButtonInMENUS.SetActive(true);
+    }
+
+    // In Menus back button
+    public void BackToMenu()
+    {
+        if (Menu1 == true || Menu2 == true || Menu3 == true)
+        {
+
+            Menu1.SetActive(false);
+            Menu2.SetActive(false);
+            Menu3.SetActive(false);
+
+            BackButtonInMENUS.SetActive(false);
+
+            MainLevelChooser.SetActive(true);
+        }
+        
+    }
+
+    // Main Menu Scene BAck Button
+    public void BackToStartMenu()
+    {
+        SceneManager.LoadScene("StartMenu");
     }
 
 
     // Level buttons
 
+    // LEVEL NATURE
     public void Level_1()
     {
-        SceneManager.LoadScene("Scene1");
+        SceneManager.LoadScene("Level_1_Nature");
     }
 
     public void Level_2()
     {
-        SceneManager.LoadScene("Scene1");
+        SceneManager.LoadScene("Level_2_Nature");
     }
 
     public void Level_3()
     {
-        SceneManager.LoadScene("Scene1");
+        SceneManager.LoadScene("Level_3_Nature");
     }
+
+    // LEVEL DESERT
 
     public void Level_4()
     {
-        SceneManager.LoadScene("Scene1");
+        SceneManager.LoadScene("Level_1_Desert");
     }
 
     public void Level_5()
     {
-        SceneManager.LoadScene("Scene1");
+        SceneManager.LoadScene("Level_2_Desert");
     }
 
     public void Level_6()
     {
-        SceneManager.LoadScene("Scene1");
+        SceneManager.LoadScene("Level_3_Desert");
     }
+
+    // LEVEL HALLOWEEN
 
     public void Level_7()
     {
-        SceneManager.LoadScene("Scene1");
+        SceneManager.LoadScene("Level_1_Halloween");
     }
 
     public void Level_8()
     {
-        SceneManager.LoadScene("Scene1");
+        SceneManager.LoadScene("Level_2_Halloween");
     }
 
     public void Level_9()
     {
-        SceneManager.LoadScene("Scene1");
+        SceneManager.LoadScene("Level_3_Halloween");
     }
 
-
-    /* OLD CONFIG
-
-
-    // Cafe level
-    public void CafeButton()
-    {
-        // load cafe
-        SceneManager.LoadScene("Level_Caffee");
-
-    }
-
-    // Magic forest level
-    public void MagicForestButton()
-    {
-        // load Magic forest
-        SceneManager.LoadScene("Level_MagicForest_Sprite");
-
-    }
-
-    // Jungle level
-    public void JungleButton()
-    {
-        // load Magic forest
-        SceneManager.LoadScene("Level_Jungle");
-
-    }
-    // Button 4 PIRATES
-    public void IslandButton()
-    {
-        SceneManager.LoadScene("Level_Island");
-    }
-
-    // Button 5 Dungeon
-    public void DungeonButton()
-    {
-        SceneManager.LoadScene("Level_Dungeon");
-    }
-
-    // Button 6 Western
-    public void WesternButton()
-    {
-        SceneManager.LoadScene("Level_Western");
-    }
-
-
-
-    public void BackButton()
-    {
-        SceneManager.LoadScene("StartMenu");
-    }
-    */
+    // END OF CODE
 }
